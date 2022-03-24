cb3abd19072515ba08fe45ca277a11699539f7e6
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2022-03-24 21:34:15 +0200

    Initial Commit

---
+++ indesign-scripts/indesign_scriptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  indesign_scriptsApp.swift
+//  indesign-scripts
+//
+//  Created by Yehuda Neufeld on 24/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct indesign_scriptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
